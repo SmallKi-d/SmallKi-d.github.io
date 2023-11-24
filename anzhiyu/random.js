1c69dacb71a9604a4d6e27f2d36903b6d3b90b7a
SmallKi-d <1502598781@qq.com>
2023-11-25 03:23:36 +0800

    Site updated: 2023-11-25 03:23:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/21/hello-world/"];function toRandomPost(){
+var posts=["2023/11/21/hello-world/","2023/11/25/在TrueNAS系统中安装PVE内核/","2023/11/25/自己的域名免费快速搭建个人博客/","2023/11/25/NAT6解决校园网IPv6分配掩码128问题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
