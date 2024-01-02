4ff3b93990a51a9a08ce679f03c4726b20c60265
SmallKi-d <1502598781@qq.com>
2024-01-02 14:52:38 +0800

    Site updated: 2024-01-02 14:52:38

--- js/delete-arrow-right.js
+++ js/delete-arrow-right.js
@@ -1,5 +1,5 @@
 $(function () {
-        console.log("进入自定义js");
+        // console.log("进入自定义js");
         let i = document.getElementsByClassName("bannerText")[0].getElementsByTagName("i")[0]
         i.remove()
         
