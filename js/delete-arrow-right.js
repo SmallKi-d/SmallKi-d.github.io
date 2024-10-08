12d2a5aab1177a6a9cf0e3d3358319ea98caa65f
Small Kid <smallkid@MacBookPro.lan>
2024-10-08 14:10:28 +0800

    Site updated: 2024-10-08 14:10:27

---
+++ js/delete-arrow-right.js
@@ -0,0 +1,6 @@
+$(function () {
+        // console.log("进入自定义js");
+        let i = document.getElementsByClassName("bannerText")[0].getElementsByTagName("i")[0]
+        i.remove()
+        
+    })
