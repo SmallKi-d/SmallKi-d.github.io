21ecdacb3d97f54d330c3e26a6d246b4c83d0d87
StrayKid <1502598781@qq.com>
2025-08-01 14:14:01 +0800

    Site updated: 2025-08-01 14:13:57

--- js 2/delete-arrow-right.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-        // console.log("进入自定义js");
-        let i = document.getElementsByClassName("bannerText")[0].getElementsByTagName("i")[0]
-        i.remove()
-        
-    })
